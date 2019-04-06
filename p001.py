0fcfd7b68c5263609d9e3c65e7935fca3387a9fa
Czerny-F <dummy00.cz@gmail.com>
2019-04-07 02:52:37 +0900

    Problem 1 efficiently

--- p001.py
+++ p001.py
@@ -3,6 +3,14 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5,
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
+
+
+def sum_div_by(n):
+    return sum(f * n for f in range(1, 999//n + 1))
+
+
 if __name__ == '__main__':
     print(sum(n for n in range(1, 1000)
               if n % 5 == 0 or n % 3 == 0))
+
+    print(sum_div_by(3) + sum_div_by(5) - sum_div_by(15))
